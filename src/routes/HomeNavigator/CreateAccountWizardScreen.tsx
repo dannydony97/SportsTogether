babf481e88f15faeb1c90859ce402d823de9def5
dannydony97 <dannydony97@gmail.com>
2023-12-20 14:22:18 +0200

    Adding each step as a render method into an array of steps

--- src/routes/HomeNavigator/CreateAccountWizardScreen.tsx
+++ src/routes/HomeNavigator/CreateAccountWizardScreen.tsx
@@ -6,6 +6,94 @@ import {Ionicon, MaterialIcon} from '../../components/Icon';
 import {Sport, UserProps} from '../../api/datamodel/types';
 import {ColorValue, ListRenderItemInfo} from 'react-native';
 
+type WizardStep = {
+  state: WizardStepStates;
+  label: string;
+  component: (userProps: UserProps, updateUserProps: (value: Partial<UserProps>) => void) => JSX.Element;
+};
+
+/**
+ * List of wizard steps
+ */
+const WIZARD_STEPS: WizardStep[] = [
+  {
+    state: WizardStepStates.ENABLED,
+    label: 'Who are you',
+    component: (userProps, updateUserProps) => (
+      <View flex>
+        <Text text30R style={{marginVertical: 20}}>
+          What's your name?
+        </Text>
+        <TextField
+          value={userProps.displayName ?? ''}
+          onChangeText={text => updateUserProps({displayName: text})}
+          placeholder="Name"
+          floatingPlaceholder
+          text50T
+        />
+      </View>
+    ),
+  },
+  {
+    state: WizardStepStates.ENABLED,
+    label: 'Preferred sports',
+    component: (userProps, updateUserProps) => {
+      /**
+       * Renders an item of the preferred sports list
+       */
+      const renderPreferredSportsListItem = (info: ListRenderItemInfo<SportItem>): JSX.Element => {
+        /**
+         * Tells if this item sport name is selected or not
+         */
+        const selected =
+          userProps.preferredSports.find(preferredSport => preferredSport === info.item.name) !== undefined;
+
+        const onListItemPress = (): void => {
+          if (!selected) {
+            updateUserProps({preferredSports: [...userProps.preferredSports, info.item.name]});
+          } else {
+            updateUserProps({
+              preferredSports: [
+                ...userProps.preferredSports.filter(preferredSport => preferredSport !== info.item.name),
+              ],
+            });
+          }
+        };
+
+        return (
+          <Chip
+            label={info.item.name}
+            leftElement={info.item.icon(selected ? Colors.$iconPrimary : Colors.$textDefault)}
+            onPress={onListItemPress}
+            labelStyle={{color: selected ? Colors.$iconPrimary : Colors.$textDefault}}
+            containerStyle={{
+              borderColor: selected ? Colors.$iconPrimary : Colors.$textDefault,
+            }}
+          />
+        );
+      };
+
+      return (
+        <View flex>
+          <Text text30R style={{marginVertical: 20}}>
+            What are your preferred sports?
+          </Text>
+          <GridList data={SPORTS} renderItem={renderPreferredSportsListItem} scrollEnabled={false} />
+        </View>
+      );
+    },
+  },
+  {
+    state: WizardStepStates.COMPLETED,
+    label: 'Complete',
+    component: () => (
+      <View flex style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Text text10>That's it!</Text>
+      </View>
+    ),
+  },
+];
+
 type SportItem = {
   name: Sport;
   icon: (color: ColorValue) => JSX.Element;
@@ -48,96 +136,6 @@ const CreateAccountWizardScreen: FC<HomeNavigatorScreenProps<'CreateAccountWizar
   };
 
   /**
-   * Renders the name view
-   */
-  const renderName = (): JSX.Element => {
-    return (
-      <View flex>
-        <Text text30R style={{marginVertical: 20}}>
-          What's your name?
-        </Text>
-        <TextField
-          value={userProps.displayName ?? ''}
-          onChangeText={text => updateUserProps({displayName: text})}
-          placeholder="Name"
-          floatingPlaceholder
-          text50T
-        />
-      </View>
-    );
-  };
-
-  /**
-   * Renders an item of the preferred sports list
-   */
-  const renderPreferredSportsListItem = (info: ListRenderItemInfo<SportItem>): JSX.Element => {
-    /**
-     * Tells if this item sport name is selected or not
-     */
-    const selected = userProps.preferredSports.find(preferredSport => preferredSport === info.item.name) !== undefined;
-
-    const onListItemPress = (): void => {
-      if (!selected) {
-        updateUserProps({preferredSports: [...userProps.preferredSports, info.item.name]});
-      } else {
-        updateUserProps({
-          preferredSports: [...userProps.preferredSports.filter(preferredSport => preferredSport !== info.item.name)],
-        });
-      }
-    };
-
-    return (
-      <Chip
-        label={info.item.name}
-        leftElement={info.item.icon(selected ? Colors.$iconPrimary : Colors.$textDefault)}
-        onPress={onListItemPress}
-        labelStyle={{color: selected ? Colors.$iconPrimary : Colors.$textDefault}}
-        containerStyle={{
-          borderColor: selected ? Colors.$iconPrimary : Colors.$textDefault,
-        }}
-      />
-    );
-  };
-
-  /**
-   * Renders the preferred sports view
-   */
-  const renderPreferredSports = (): JSX.Element => {
-    return (
-      <View flex>
-        <Text text30R style={{marginVertical: 20}}>
-          What are your preferred sports?
-        </Text>
-        <GridList data={SPORTS} renderItem={renderPreferredSportsListItem} scrollEnabled={false} />
-      </View>
-    );
-  };
-
-  /**
-   * Renders the completed view
-   */
-  const renderCompleted = (): JSX.Element => {
-    return (
-      <View flex style={{alignItems: 'center', justifyContent: 'center'}}>
-        <Text text10>That's it!</Text>
-      </View>
-    );
-  };
-
-  const renderCurrentStep = (): JSX.Element => {
-    switch (activeIndex) {
-      case 0:
-        return renderName();
-      case 1:
-        return renderPreferredSports();
-      case 2:
-        return renderCompleted();
-      default:
-        throw new Error(`Step ${activeIndex} has no render function`);
-    }
-  };
-
-  /**
    * Tells if the 'next' button is disabled or not depending on the current step
    */
   const isDisabledNextButton = (): boolean => {
@@ -154,17 +152,19 @@ const CreateAccountWizardScreen: FC<HomeNavigatorScreenProps<'CreateAccountWizar
   return (
     <Screen>
       <Wizard activeIndex={activeIndex}>
-        <Wizard.Step state={WizardStepStates.ENABLED} label={'Who are you'} />
-        <Wizard.Step state={WizardStepStates.ENABLED} label={'Preferred sports'} />
-        <Wizard.Step state={WizardStepStates.COMPLETED} label={'Complete'} />
+        {WIZARD_STEPS.map(({state, label}) => (
+          <Wizard.Step state={state} label={label} />
+        ))}
       </Wizard>
       <View flex style={{padding: 15}}>
-        {renderCurrentStep()}
+        {WIZARD_STEPS[activeIndex].component(userProps, updateUserProps)}
         <View style={{justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'row'}}>
-          <View flex style={{flexDirection: 'row', alignItems: 'center'}}>
-            <MaterialIcon name="info" size={30} />
-            <Text>You can change this later in settings.</Text>
-          </View>
+          {WIZARD_STEPS[activeIndex].state !== WizardStepStates.COMPLETED && (
+            <View flex style={{flexDirection: 'row', alignItems: 'center'}}>
+              <MaterialIcon name="info" size={30} />
+              <Text>You can change this later in settings.</Text>
+            </View>
+          )}
           <Button
             style={{padding: 13}}
             iconSource={() => <MaterialIcon name="arrow-forward-ios" size={30} color="white" />}
