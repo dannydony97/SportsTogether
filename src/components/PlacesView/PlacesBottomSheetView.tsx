617c66e0768e58e8e00740e4878a9256f300a7d5
dannydony97 <dannydony97@gmail.com>
2023-12-24 13:42:25 +0200

    Bottom sheet progress

--- src/components/PlacesView/PlacesBottomSheetView.tsx
+++ src/components/PlacesView/PlacesBottomSheetView.tsx
@@ -1,8 +1,11 @@
 import React, {FC, useEffect, useMemo, useRef} from 'react';
 import {PlacesBottomSheetViewProps} from './types';
-import {Text, View} from 'react-native-ui-lib';
-import {BottomSheetModal} from '@gorhom/bottom-sheet';
+import {Image, Text, View} from 'react-native-ui-lib';
+import {BottomSheetModal, BottomSheetScrollView} from '@gorhom/bottom-sheet';
 import {BottomSheetModalMethods} from '@gorhom/bottom-sheet/lib/typescript/types';
+import {FlatList} from 'react-native-gesture-handler';
+
+const ICON_IMAGE_SIZE = 70;
 
 const PlacesBottomSheetView: FC<PlacesBottomSheetViewProps> = ({placeData}) => {
   /**
@@ -13,7 +16,7 @@ const PlacesBottomSheetView: FC<PlacesBottomSheetViewProps> = ({placeData}) => {
   /**
    * Snap points
    */
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const snapPoints = useMemo(() => ['25%', '50%', '75%'], []);
 
   useEffect(() => {
     if (placeData) {
@@ -23,11 +26,43 @@ const PlacesBottomSheetView: FC<PlacesBottomSheetViewProps> = ({placeData}) => {
     }
   }, [placeData]);
 
+  /**
+   * Renders images from the flat list
+   * @param image image uri
+   */
+  const renderImage = (image: string): JSX.Element => {
+    return (
+      <View style={{padding: 10}}>
+        <Image source={{uri: image}} style={{width: 200, aspectRatio: 2 / 3, borderRadius: 10}} />
+      </View>
+    );
+  };
+
   return (
     <BottomSheetModal enablePanDownToClose={false} ref={bottomSheetRef} snapPoints={snapPoints} index={1}>
-      <View>
-        <Text>Awesome 🎉</Text>
-      </View>
+      <BottomSheetScrollView>
+        <View flex style={{flexDirection: 'row', paddingHorizontal: 10, marginBottom: 10}}>
+          <Image
+            style={{
+              width: ICON_IMAGE_SIZE,
+              height: ICON_IMAGE_SIZE,
+              borderRadius: ICON_IMAGE_SIZE / 2,
+              marginRight: 10,
+            }}
+            source={{uri: placeData?.images[0]}}
+          />
+          <View>
+            <Text text50BO>{placeData?.name}</Text>
+            <Text text80>{placeData?.sport}</Text>
+          </View>
+        </View>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={placeData?.images ?? []}
+          renderItem={info => renderImage(info.item)}
+        />
+      </BottomSheetScrollView>
     </BottomSheetModal>
   );
 };
